26ead2b47e5fa8716599183b383aa80913086e88
aoyou <aoyoumx@126.com>
2025-09-06 18:53:02 +0800

    feat(搭建): readme

--- test.js
+++ test.js
@@ -1,20 +1,20 @@
-const cal = (x, n) => {
-    // 
-    if (n === 0) return 1;
-    let res = 1;
-    for (let i = 0; i < n; i++) {
-        res *= x
-    }
-    if (res > Number.MAX_VALUE) {
-        return Number.MAX_VALUE
-    }
-    if (res < Number.MIN_VALUE) {
-        return Number.MIN_VALUE
-    }
-    return res
-}
+// const cal = (x, n) => {
+//     // 
+//     if (n === 0) return 1;
+//     let res = 1;
+//     for (let i = 0; i < n; i++) {
+//         res *= x
+//     }
+//     if (res > Number.MAX_VALUE) {
+//         return Number.MAX_VALUE
+//     }
+//     if (res < Number.MIN_VALUE) {
+//         return Number.MIN_VALUE
+//     }
+//     return res
+// }
 
-console.log(cal(x, 8))
+// console.log(cal(x, 8))
 
 // sign = x*x
 // i < 8/2
@@ -26,3 +26,16 @@ console.log(cal(x, 8))
 //     res1 *= res
 //     res1 = cal(res1, n/2)
 // }
+
+const reverseNode = (node) => {
+    let prev = null
+    let curr = node
+    while(curr) {
+        const tempNext = curr.next;
+        curr.next = prev
+        prev = curr
+        // last
+        curr = tempNext
+    }
+    return prev
+}
